fa1ed9ab29fa1b4ef6776034943237628ebc8c48
biasmj016 <mjkim031@gmail.com>
2024-12-21 21:30:18 +0900

    project init

---
+++ src/main/java/com/translation/support/I18nSupportApplication.java
@@ -0,0 +1,13 @@
+package com.translation.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18nSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I18nSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/translation/support/I18nSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.translation.support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class I18nSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
