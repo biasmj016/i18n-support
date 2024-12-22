310537a3784f9d56bd3728459451e77e5071b777
biasmj016 <mjkim031@gmail.com>
2024-12-22 21:06:08 +0900

    toResponse 메소드 추가

--- src/main/java/com/translation/support/domain/Translation.java
+++ src/main/java/com/translation/support/domain/Translation.java
@@ -1,8 +1,19 @@
 package com.translation.support.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.translation.support.infrastructure.in.web.response.TextTranslateResponse;
+
+import static com.translation.support.domain.SourceLanguage.fromCodeOrName;
 
 public record Translation(
         @JsonProperty("text") String text,
         @JsonProperty("detected_source_language") String sourceLanguage
-) { }
+) {
+    public TextTranslateResponse toResponse(String targetLanguage) {
+        return new TextTranslateResponse(
+                text,
+                fromCodeOrName(targetLanguage).name(),
+                fromCodeOrName(sourceLanguage).name()
+        );
+    }
+}
